2640eed1261a4a4b7c43f7986ee32d4ac30199dd
Jared Mauch <jared@puck.nether.net>
2025-02-28 12:16:24 +0000

    add report upload, move large block of main to function

--- pdig-dns-tool.py
+++ pdig-dns-tool.py
@@ -8,11 +8,13 @@
 """
 
 import argparse
+import os
 import socket
 import statistics
 import sys
 import time
 import tempfile
+import requests
 
 # 3rd party imports
 try:
@@ -33,8 +35,7 @@ except:
 #    print("apt install python3-netifaces or pip3 install netifaces")
 #    sys.exit(0)
 
-all_ips = {}
-socket_af_types = [socket.AF_INET, socket.AF_INET6]
+
 
 # full_qname: domain name to query for
 # prev_cache: list of nameservers to query
@@ -42,7 +43,7 @@ socket_af_types = [socket.AF_INET, socket.AF_INET6]
 #     eg: [dns.rdatatype.A, dns.rdatatype.AAAA]
 # tcp: when true, send query over tcp
 #
-def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
+def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle, high_latency, ip_list):
     cname_reply = None
     new_cache = []
     times = []
@@ -54,6 +55,8 @@ def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
             q = dns.message.make_query(full_qname, qtype)
         except Exception as e:
             print(e, full_qname, qtype)
+            if file_handle is not None:
+                os.write(file_handle, str.encode(f"{e}:{full_qname}:{qtype}\n"))
         for x in prev_cache:
             qip = x['addrinfo']
             # avoid possible odd link-local issues
@@ -64,39 +67,44 @@ def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
                 query_ip[qip + str(qtype)] = 1
 
                 # store list of all IPs
-                all_ips[qip] = 1
+                ip_list[qip] = 1
 
                 # timer
                 start_time = time.time()
                 try:
                     if tcp:
-                        resp = dns.query.tcp(q, x['addrinfo'], timeout=10)
+                        resp = dns.query.tcp(q, qip, timeout=10)
                     else:
-                        resp =  dns.query.udp(q, x['addrinfo'], timeout=10)
+                        resp =  dns.query.udp(q, qip, timeout=10)
                     stop_time = time.time()
 
                     latency = stop_time - start_time
                     latency_ms = latency * 1000
                     times.append(latency_ms)
 
-                    print(f"ns={x['qname']} addr={x['addrinfo']}, latency={latency_ms:.3f} ms")
-                    if file_handle is not None:
-                        file_handle.write(f"ns={x['qname']} addr={x['addrinfo']}, latency={latency_ms:.3f} ms" + '\n')
+                    if latency_ms > 100 or high_latency is False:
+                        print(f"ns={x['qname']} addr={qip}, latency={latency_ms:.3f} ms")
+                        if file_handle is not None:
+                            os.write(file_handle, str.encode(f"ns={x['qname']} addr={qip}, latency={latency_ms:.3f} ms" + '\n'))
+
                     if resp.rcode() == dns.rcode.NXDOMAIN:
                         domain_exists = False
                         continue
                     if resp.rcode() != dns.rcode.NOERROR:
-                        print(f"{dns.rcode.to_text(resp.rcode())} for {full_qname} at {x['addrinfo']}")
+                        print(f"{dns.rcode.to_text(resp.rcode())} for {full_qname} at {qip}")
                         continue
-                    if len(resp.answer) > 0:
-                        # 5 = CNAME rfc1035
-                        if resp.answer[0].rdtype == 5:
-                            for i in resp.answer[0].items:
-                                cname_reply = str(i)
 
-                    # parse the authority portion of response packet
+                    # parse the response packet
                     # if we are not yet to an authoritative server
-                    if not resp.flags & dns.flags.AA:
+                    if not resp.flags & dns.flags.AA or len(resp.answer) > 0:
+                        for var in resp.answer:
+                            for i in var.items:
+                                if var.rdtype == dns.rdatatype.CNAME:
+                                    cname_reply = str(i)
+                                if file_handle is not None:
+                                    os.write(file_handle, str.encode(f"\"{latency_ms}\";\"{qip}\";{i}" + '\n'))
+                                print(f"\"{latency_ms}\";\"{qip}\";{i}")
+                        # parse the authority portion of response packet
                         for var in resp.authority:
                             for i in var.items:
                                 # check NS responses
@@ -113,12 +121,18 @@ def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
                                             addr_list = a[4]
                                             new_cache.append({'qname': str_name, 'af_type': a[0], 'addrinfo': addr_list[0]})
                 except dns.query.BadResponse as e:
-                    print(f"error {e} querying {x['addrinfo']} for {full_qname}")
+                    print(f"error {e} querying {qip} for {full_qname}")
+                    if file_handle is not None:
+                        os.write(file_handle, str.encode(f"error {e} querying {qip} for {full_qname}\n"))
                 except dns.exception.Timeout as e:
                     print(f"timeout querying: {x['addrinfo']}")
+                    if file_handle is not None:
+                        os.write(file_handle, str.encode(f"timeout querying: {qip}\n"))
                 except OSError as e:
                     # This is the new block to catch network unreachable errors
-#                    print(f"Network error: {e} when trying to reach {x['addrinfo']}")
+                    print(f"Network error: {e} when trying to query {qip} for {x['qname']}")
+                    if file_handle is not None:
+                        os.write(file_handle, str.encode(f"Network error: {e} when trying to query {qip} for {x['qname']}"))
                     continue  # Skip this address and try the next one
 
     # output some statistics at the end
@@ -129,15 +143,15 @@ def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
     stddev = 0 if len(times) < 2 else statistics.stdev(times)
     min_max_ratio = 0 if min_value == 0 else max_value / min_value
     print(f"latency: min={min_value:.3f} ms max={max_value:.3f} ms avg={avg_value:.3f} ms")
-    print(f"stdev={stddev:.3f} ms max-min={min_max_range:.3f} max/min={min_max_ratio:.2f} x latency variance")
+    print(f"stdev={stddev:.3f} ms max-min={min_max_range:.3f} ms max/min={min_max_ratio:.2f} x latency variance")
     if file_handle is not None:
-        file_handle.write(f"latency: min={min_value:.3f} ms max={max_value:.3f} ms avg={avg_value:.3f} ms" + '\n')
-        file_handle.write(f"stdev={stddev:.3f} ms max-min={min_max_range:.3f} max/min={min_max_ratio:.2f} x latency variance" + '\n')
+        os.write(file_handle, str.encode(f"latency: min={min_value:.3f} ms max={max_value:.3f} ms avg={avg_value:.3f} ms" + '\n'))
+        os.write(file_handle, str.encode(f"stdev={stddev:.3f} ms max-min={min_max_range:.3f} ms max/min={min_max_ratio:.2f} x latency variance" + '\n'))
 
     if not domain_exists:
         print(f"NXDOMAIN for {full_qname}, stopping...")
         if file_handle is not None:
-            file_handle.write(f"NXDOMAIN for {full_qname}, stopping..." + '\n')
+            os.write(file_handle, str.encode(f"NXDOMAIN for {full_qname}, stopping..." + '\n'))
         return ([], None)
     # See bug #5. This is to prevent some endless loops if we do not
     # progress in the domain name tree.
@@ -146,16 +160,95 @@ def query_all(full_qname, prev_cache, qtype_list, tcp, file_handle):
         new_cache = []
     return (new_cache, cname_reply)
 
-root_hints = []
+
+def query_domain(fqdn, cli_args, socket_types):
+    root_hints = []
+
+    all_ips = {}
+
+    print(f"querying for {fqdn}")
+
+    fd = None
+    filename = None
+    if cli_args.report:
+        (fd, filename) = tempfile.mkstemp(suffix=".txt", text=True)
+        os.write(fd, str.encode(f"querying for {fqdn}" + '\n'))
+
+    ts = time.ctime()
+    print(f"start={ts}")
+    if fd is not None:
+        os.write(fd, str.encode(f"start={ts}" + '\n'))
+
+    # preseed the data
+    response = dns.resolver.resolve(".", "NS", lifetime=10, tcp=use_tcp)
+    for var in response.response.answer:
+        for i in var.items:
+            for fam in socket_types:
+                try:
+                    add_info = socket.getaddrinfo(host=i.to_text(), port=None, family=fam, proto=socket.SOCK_RAW)
+                except socket.gaierror:
+                    continue
+                str_name = str(i.to_text())
+                for a in add_info:
+                    addr_list = a[4]
+                    root_hints.append({'qname': str_name, 'af_type': a[0], 'addrinfo': addr_list[0]})
+#                    print(f"getaddrinfo: {str_name}: af_type: {a[0]}, addrinfo: {addr_list[0]}")
+    old_cache = root_hints
+
+    # run through the domain tree until done
+    while len(old_cache) > 0:
+        (reply_hints, new_domain) = query_all(fqdn, old_cache, [dns.rdatatype.TXT], use_tcp, fd, cli_args.gt, all_ips)
+        old_cache = reply_hints
+        if new_domain is not None:
+            print(f"(re)querying for {fqdn} due to CNAME to {new_domain}")
+            if fd is not None:
+                os.write(fd, str.encode(f"(re)querying for {fqdn} due to CNAME to {new_domain}" + '\n'))
+            fqdn = new_domain
+            old_cache = root_hints
+        print("===================")
+        if fd is not None:
+            os.write(fd, str.encode("===================" + "\n"))
+
+    ts = time.ctime()
+    print(f"end={ts}")
+    if fd is not None:
+        os.write(fd, str.encode(f"end={ts}" + '\n'))
+
+        for ip in all_ips:
+            os.write(fd, str.encode(f"# {ip}\n"))
+            os.write(fd, str.encode(f"dig +noall +answer +stats @{ip} identity.nameserver.id ch txt\n"))
+            identity = dns.message.make_query("identity.nameserver.id", dns.rdatatype.TXT, rdclass=dns.rdataclass.CHAOS)
+            try:
+                resp =  dns.query.udp(identity, ip, timeout=10)
+                for var in resp.answer:
+                    for i in var.items:
+                        os.write(fd, str.encode(f"\"{ts}\";\"{ip}\";{i}" + '\n'))
+                        print(f"\"{ts}\";\"{ip}\";{i}")
+            except Exception as e:
+                print(f"{e}:{ip}")
+                os.write(fd, str.encode(f"{e}:{ip}"))
+            os.write(fd, str.encode(f"mtr -bw {ip}\n"))
+
+    if fd is not None:
+        os.close(fd)
+        print(filename)
+    return filename
+# end query_domain
 
 # main()
 
+# define a parser
 parser = argparse.ArgumentParser(prog=sys.argv[0])
 parser.add_argument('domain', help="domain name to query") # positional argument
 parser.add_argument('-6', '--ipv6', action='store_true', help="query ipv6-only") # ipv6-only
 parser.add_argument('-4', '--ipv4', action='store_true', help="query ipv4-only") # ipv4-only
 parser.add_argument('-t', '--tcp', action='store_true', help="send queries over TCP") # use TCP
 parser.add_argument('-r', '--report', action='store_true', help="Save results to file")
+parser.add_argument('-g', '--gt', action='store_true', help="greater than 100ms only")
+parser.add_argument('-u', '--upload', action='store_true', help="requires -r - uploads report to hardcoded url")
+
+
+socket_af_types = [socket.AF_INET, socket.AF_INET6]
 
 args = parser.parse_args()
 if args.ipv4:
@@ -170,63 +263,16 @@ else:
 #parse args
 domain = args.domain
 
-print(f"querying for {domain}")
+# XXX Replace me if you are going to use -u flag
+url = "https://www.example.com/upload/upload_file.php"
 
-fh = None
-if args.report:
-    temp_name = '/tmp/' + next(tempfile._get_candidate_names())
-    print(temp_name)
-    fh = open(temp_name, "w", encoding='ascii')
-    fh.write(f"querying for {domain}" + '\n')
-
-ts = time.ctime()
-print(f"start={ts}")
-if fh is not None:
-    fh.write(f"end={ts}" + '\n')
-
-# preseed the data
-response = dns.resolver.resolve(".", "NS", lifetime=10, tcp=use_tcp)
-for var in response.response.answer:
-    for i in var.items:
-        for fam in socket_af_types:
-            try:
-                add_info = socket.getaddrinfo(host=i.to_text(), port=None, family=fam, proto=socket.SOCK_RAW)
-            except socket.gaierror:
-                continue
-            str_name = str(i.to_text())
-            for a in add_info:
-                addr_list = a[4]
-                root_hints.append({'qname': str_name, 'af_type': a[0], 'addrinfo': addr_list[0]})
-#                print(f"getaddrinfo: {str_name}: af_type: {a[0]}, addrinfo: {addr_list[0]}")
-old_cache = root_hints
-
-# run through the domain tree until done
-while len(old_cache) > 0:
-    (reply_hints, new_domain) = query_all(domain, old_cache, [dns.rdatatype.TXT], use_tcp, fh)
-    old_cache = reply_hints
-    if new_domain is not None:
-        print(f"(re)querying for {domain} due to CNAME")
-        if fh is not None:
-            fh.write(f"(re)querying for {domain} due to CNAME" + '\n')
-        domain = new_domain
-        old_cache = root_hints
-    print("===================")
-    if fh is not None:
-        fh.write("===================" + "\n")
-#
-
-ts = time.ctime()
-print(f"end={ts}")
-if fh is not None:
-    fh.write(f"end={ts}" + '\n')
-
-    for ip in all_ips:
-        fh.write(f"# {ip}\n")
-        fh.write(f"dig +noall +answer +stats @{ip} identity.nameserver.id ch txt\n")
-        fh.write(f"mtr -bw {ip}\n")
-
-if fh is not None:
-    fh.close()
-    print(temp_name)
+fn = query_domain(domain, args, socket_af_types)
+if fn is not None:
+    print(f"fn={fn}")
+    if args.upload:
+        post_response = requests.post(url, data={'file': fn}, files={'file': open(fn, "rb")}, timeout=10)
+        print("post=", post_response.text)
+    # remove temporary file
+    os.unlink(fn)
 
 #
